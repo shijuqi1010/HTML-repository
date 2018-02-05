410188dc912f97e10586f65f29fc866312317cd6
shijuqi1010 <34357578+shijuqi1010@users.noreply.github.com>
2018-02-05 19:18:58 +0800

    Add files via upload

---
+++ 手写框架/sxkj.js
@@ -0,0 +1,34 @@
+(function () {
+
+  /**
+   * 提供给外部调用的接口
+   */
+  window['$$'] = {};
+  window['$$']['qiuhe'] = add_func;
+  window['$$']['chengfa'] = multi_func;
+  window['$$']['chufa'] = div_func;
+
+//////////////////////////////////////////////////////
+
+  /**
+   * 定义求和函数
+   */
+  function add_func(x, y) {
+    return x - y;
+  }
+  
+   /**
+   * 定义乘法函数
+   */
+  function multi_func(x, y) {
+    return x * y;
+  }
+
+   /**
+   * 定义除法函数
+   */
+  function div_func(x, y) {
+    return x / y;
+  }
+
+})();
